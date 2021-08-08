b8dce9b60be301e6844c5fa65705164910b8ad5b
fedorchukdmytro <fedorchuk.dmytro@ukr.net>
2021-08-08 09:05:40 +0300

    all done

--- group/models.py
+++ group/models.py
@@ -1,9 +1,6 @@
 from django.db import models
-from django.core.validators import MaxValueValidator
-from django.core.validators import MinValueValidator
 
 
 class Group(models.Model):
     descipline= models.CharField(max_length=200)
-    hours_to_take = models.IntegerField(validators=[MaxValueValidator(30),MinValueValidator(10)])
-    
+    hours_to_take = models.IntegerField(default=32)
