ed313c87e17ceb924b03de488d631625a65f14ac
fedorchukdmytro <fedorchuk.dmytro@ukr.net>
2021-08-27 21:45:35 +0300

    done

--- students/models.py
+++ students/models.py
@@ -6,7 +6,7 @@ class Student(models.Model):
     last_name = models.CharField(max_length=200)
     age = models.IntegerField(default=18)
     phone = models.CharField(max_length=15, null=True)
-    
+
     def __str__(self):
         return f"{self.first_name} {self.last_name} {self.age} {self.phone}"
 
