f1ddafc8f589aaff92588f3e0a6f411e3449b9cd
fedorchukdmytro <fedorchuk.dmytro@ukr.net>
2021-10-20 01:03:23 +0300

    upt

--- django_fedorchuk/settings.py
+++ django_fedorchuk/settings.py
@@ -23,10 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r8p=+a)6$s1cc5**e#=!2l90z$1r!x*r$3gbgep(!ih&zm%-zf'
-
+# SECRET_KEY = 'django-insecure-r8p=+a)6$s1cc5**e#=!2l90z$1r!x*r$3gbgep(!ih&zm%-zf'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+
+
 
 ALLOWED_HOSTS = ['*']
 
