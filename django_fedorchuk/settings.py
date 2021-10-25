c8d7ac728e16cbbea2e818e673b8dbf3a0b7f738
fedorchukdmytro <fedorchuk.dmytro@ukr.net>
2021-10-26 00:57:15 +0300

    q

--- django_fedorchuk/settings.py
+++ django_fedorchuk/settings.py
@@ -114,7 +114,7 @@ WSGI_APPLICATION = 'django_fedorchuk.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresgl',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
