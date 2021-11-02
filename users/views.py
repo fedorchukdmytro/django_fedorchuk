c0175064b9175da1d3e76c493f867fbb0d25f1ba
fedorchukdmytro <fedorchuk.dmytro@ukr.net>
2021-11-02 20:17:34 +0200

    sf

--- users/views.py
+++ users/views.py
@@ -43,7 +43,7 @@ def password_reset_request(request):
 					email_template_name = "password/password_reset_email.txt"
 					c = {
 					"email":user.email,
-					'domain':'dfedorchuk.herokuapp.com/',
+					'domain':'dfedorchuk.herokuapp.com',
 					'site_name': 'Website',
 					"uid": urlsafe_base64_encode(force_bytes(user.pk)),
 					"user": user,
